f87991afdcf6ef8b69427d3ddf9d755f4f0c368b
Alex Chernov <fastshadow@inbox.ru>
2022-04-19 01:20:26 +0300

    update

--- src/test/java/com/epam/student/ticketservice/testControllers/TestUserController.java
+++ src/test/java/com/epam/student/ticketservice/testControllers/TestUserController.java
@@ -1,26 +1,27 @@
 package com.epam.student.ticketservice.testControllers;
 
-import com.epam.student.ticketservice.entity.UserEntity;
+import com.epam.student.ticketservice.model.User;
 import com.epam.student.ticketservice.repository.UserRepository;
 import com.epam.student.ticketservice.service.UserService;
-import com.epam.student.ticketservice.web.UserController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -39,59 +40,68 @@ public class TestUserController {
        private ObjectMapper mapper;
        @Autowired
        private MockMvc mockMvc;
-       @Autowired
-       private UserController userController;
 
        @Test
-       public void shouldGetNoUserWhenNoCreated() throws Exception {
-              mockMvc.perform(get("/users"))
+       public void test_GetAllUsers () throws Exception {
+              when(userService.getAllUsers()).
+                      thenReturn(List.of(new User(1L, "Ivan", "Petrov", "220555", null, false),
+                                         new User(2L, "Sidor", "Ivanov", "235789", null, false)));
+              mockMvc
+                      .perform(MockMvcRequestBuilders.get("/users"))
+                      .andDo(print())
+                      .andExpect(MockMvcResultMatchers.status().isOk())
+                      .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2))
+                      .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                      .andExpect(MockMvcResultMatchers.jsonPath("$[0].lastname").value("Petrov"))
+                      .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                      .andExpect(MockMvcResultMatchers.jsonPath("$[1].firstname").value("Sidor"))
+                      .andExpect(MockMvcResultMatchers.jsonPath("$[1].passport").value("235789"));
+       }
+
+
+       @Test
+       public void test_GetNoUserWhenNoCreated() throws Exception {
+             mockMvc.perform(get("/users"))
                       .andDo(print()).andExpect(status().isOk())
                       .andExpect(jsonPath("$").isEmpty());
        }
 
        @Test
-       public void shouldGetUserWhenCreated() throws Exception {
-              UserEntity userEntity = new UserEntity(null, "Ivan", "Petrov", "220555", null, false);
-              Mockito.when(userRepository.save(Mockito.any(UserEntity.class)))
-                      .thenReturn(userEntity);
+       public void test_GetUserById() throws Exception {
+              User user = new User(1L, "Ivan", "Petrov", "220555", null, false);
+              when(userService.getUserById(user.getId())).thenReturn(user);
 
+             mockMvc.perform(MockMvcRequestBuilders
+                      .get("/users/1"))
+                      .andExpect(status().isOk())
+                      .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                      .andExpect(MockMvcResultMatchers.jsonPath("$.firstname").value("Ivan"));
 
-              MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/users")
-                      .contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON)
-                      .characterEncoding("UTF-8").content(this.mapper.writeValueAsString(userEntity));
-
-              mockMvc.perform(builder).andDo(print()).andExpect(status().isOk());//.andExpect(jsonPath("$.firstname",is ("Ivan")))
-              //      .andExpect(MockMvcResultMatchers.content().string(this.mapper.writeValueAsString(userEntity)));
        }
-
-
-//              mockMvc.perform(post("/users")
-//                      .contentType(MediaType.APPLICATION_JSON)
-//                      .content(mapper.writeValueAsString(userEntity)))
-//
-//       //               .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-////                      .characterEncoding("utf-8"))
-////                      .andDo(print())
-//                      .andExpect(status().isOk())
-//
-//                     .andExpect(MockMvcResultMatchers.jsonPath("$[0].id",is(1L)))
-//                      .andExpect(jsonPath("$.firstname",is("Ivan")));
-//              verify(userRepository, times(1)).save(any(UserEntity.class));
 
        @Test
-       public void getPatientById_success() throws Exception {
-              UserEntity userEntity = new UserEntity(1L, "Ivan", "Petrov", "220555", null, false);
-              Mockito.when(userRepository.findById(userEntity.getId())).thenReturn(java.util.Optional.of(userEntity));
+       public void test_AddUser() throws Exception {
+              User user = new User(1L, "Ivan", "Petrov", "220555", null, false);
 
-              mockMvc.perform(MockMvcRequestBuilders
-                      .get("/users/1")
-                      .contentType(MediaType.APPLICATION_JSON))
+                    mockMvc.perform(post("/users")
+                      .content(mapper.writeValueAsString(user))
+                      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                      .andDo(print())
                       .andExpect(status().isOk());
-//                      .andExpect(jsonPath("$", notNullValue()))
-//                      .andExpect(jsonPath("$.firstname", is("Ivan")));
+       }
+
+       @Test
+       public void test_EditUser() throws Exception {
+              User userUpdate = new User(1L, "Ivan", "Kozlov", "220555", null, false);
+
+              mockMvc.perform(put("/users/1")
+                      .content(mapper.writeValueAsString(userUpdate))
+                      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                      .andDo(print())
+                      .andExpect(status().isOk());
 
        }
 
-}
 
+     }
 
